be20fed96f4c2f3cd559882a3429e7db4887e7c1
Vedant <vedantnp200@gmail.com>
2023-01-09 22:13:36 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Responsive Navigation
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+// 
